b22427371e751718c16e4d1597747b563f135830
naubull2 <feasible@kakao.com>
2023-10-02 21:21:36 +0900

    [rollback] torch.cuda autocast causes half precision error
    + Works fine without the torch.cuda autocast context, so rollback.

--- fine-tune.py
+++ fine-tune.py
@@ -182,16 +182,15 @@ def train():
         # enable trainable params
         [p.requires_grad_() for n, p in model.named_parameters() if any([k in n for k in training_args.trainable_params.split(",")])]
 
-    with torch.cuda.amp.autocast(dtype=model.dtype):
-        model.config.use_cache = False         # required for gradient checkpointing
-        model.enable_input_require_grads()     # required for gradient checkpointing
-        model.gradient_checkpointing_enable()  # enable gradient checkpointing
-        trainer = Trainer(
-            model=model, tokenizer=tokenizer, args=training_args, 
-            train_dataset=dataset["train"],
-            eval_dataset=None,
-            data_collator=data_collator)
-        trainer.train()
+    model.config.use_cache = False         # required for gradient checkpointing
+    model.enable_input_require_grads()     # required for gradient checkpointing
+    model.gradient_checkpointing_enable()  # enable gradient checkpointing
+    trainer = Trainer(
+        model=model, tokenizer=tokenizer, args=training_args, 
+        train_dataset=dataset["train"],
+        eval_dataset=None,
+        data_collator=data_collator)
+    trainer.train()
     trainer.save_state()
     trainer.save_model(output_dir=training_args.output_dir)
 
