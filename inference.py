4a8464f221d7358011183925853541f2ed8a7ced
yukang <yukangchen@cse.cuhk.edu.hk>
2023-10-03 11:12:16 +0800

    Update inference.py

--- inference.py
+++ inference.py
@@ -74,7 +74,7 @@ def build_generator(
 
 def main(args):
     if args.flash_attn:
-        replace_llama_attn()
+        replace_llama_attn(inference=True)
 
     # Set RoPE scaling factor
     config = transformers.AutoConfig.from_pretrained(
