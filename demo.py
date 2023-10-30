a599f7243c1ec66152940fe6b78915f493326dbd
yukang <yukangchen@cse.cuhk.edu.hk>
2023-10-30 23:24:42 +0800

    Update demo.py

--- demo.py
+++ demo.py
@@ -65,6 +65,12 @@ PROMPT_DICT = {
         "Write a response that appropriately completes the request.\n\n"
         "### Instruction:\n{instruction}\n\n### Response:"
     ),
+    "prompt_no_input_llama2":(
+        "<s>[INST] <<SYS>>\n"
+        "You are a helpful, respectful and honest assistant. Always answer as helpfully as possible, while being safe.  Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content. Please ensure that your responses are socially unbiased and positive in nature.\n\n"
+        "If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information.\n"
+        "<</SYS>> \n\n {instruction} [/INST]"
+    ),
 }
 
 
@@ -86,7 +92,7 @@ def build_generator(
             return "Only support txt file."
 
         material = read_txt_file(material.name)
-        prompt_no_input = PROMPT_DICT["prompt_no_input"]
+        prompt_no_input = PROMPT_DICT["prompt_no_input_llama2"]
         prompt = prompt_no_input.format_map({"instruction": material + "\n%s" % question})
 
         inputs = tokenizer(prompt, return_tensors="pt").to(model.device)
